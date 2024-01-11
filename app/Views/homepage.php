a429d5dad917161b4688c71a44a0cd2e40021248
Brogalang <brogalangsatria@gmail.com>
2024-01-12 06:34:16 +0700

    footer finish

--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -26,6 +26,10 @@
          font-family: 'Poppins', sans-serif;
          color: #ef001d;
          }
+         .custom-font-white {
+         font-family: 'Poppins', sans-serif;
+         color: #ffffff ;
+         }
          .custom-font {
          font-family: 'Poppins', sans-serif;
          }
@@ -701,10 +705,10 @@
       <footer class="section-red text-inverse">
         <div class="container py-13 py-md-15">
           <div class="row gy-6 gy-lg-0">
-            <div class="col-md-12 col-lg-2">
+            <div class="col-md-12 col-lg-3">
               <div class="widget">
                 <img src="<?php echo base_url('template') ?>/assets/img/gsi/LOGO GENOMIC SHRIMP PUTIH.png" srcset="<?php echo base_url('template') ?>/assets/img/gsi/LOGO GENOMIC SHRIMP PUTIH.png 2x" alt="" />
-                <p class="mb-5 custom-font text-white">Slogan Genomic Shrimp Indonesia</p>
+                <p class="mb-5 custom-font-white">Slogan Genomic Shrimp Indonesia</p>
                 <div class="newsletter-wrapper">
                 </div>
                 <!-- /.newsletter-wrapper -->
@@ -717,13 +721,14 @@
             <!-- /column -->
             <div class="col-md-4 col-lg-3">
               <div class="widget">
-                <h4 class="widget-title text-white mb-3">Learn More</h4>
-                <ul class="list-unstyled  mb-0">
-                  <li><a href="#">About Us</a></li>
-                  <li><a href="#">Our Story</a></li>
-                  <li><a href="#">Projects</a></li>
-                  <li><a href="#">Terms of Use</a></li>
-                  <li><a href="#">Privacy Policy</a></li>
+                <h4 class="widget-title mb-3 custom-font-white">Menu</h4>
+                <ul class="list-unstyled mb-0">
+                  <li><a class="scroll custom-font-white" href="#beranda">Beranda</a></li>
+                  <li><a class="scroll custom-font-white" href="#product">Produk</a></li>
+                  <li><a class="scroll custom-font-white" href="#testimoni">Testimoni</a></li>
+                  <li><a class="scroll custom-font-white" href="#tentangKami">Tentang Kami</a></li>
+                  <li><a class="scroll custom-font-white" href="#">Privacy Policy</a></li>
+                  <li><a class="scroll custom-font-white" href="#">Term & Condition</a></li>
                 </ul>
               </div>
               <!-- /.widget -->
@@ -731,22 +736,29 @@
             <!-- /column -->
             <div class="col-md-4 col-lg-3">
               <div class="widget">
-                <h4 class="widget-title text-white mb-3">Get in Touch</h4>
-                <address class="pe-xl-15 pe-xxl-17">Moonshine St. 14/05 Light City, London, United Kingdom</address>
-                <a href="mailto:#">info@email.com</a><br /> 00 (123) 456 78 90
+                <h4 class="widget-title custom-font-white mb-3">Penerimaan Sample</h4>
+                <p class="pe-xl-15 pe-xxl-17 custom-font-white">Senin - Minggu<br>08.30 - 17.30</p>
+              </div>
+              <div class="widget">
+                <h4 class="widget-title custom-font-white mb-3">Lokasi</h4>
+                <address class="pe-xl-15 pe-xxl-17 custom-font-white">Genomics Hub Jl. Sultan Agung No.29, Guntur, Setiabudi, Jakarta Selatan, DKI Jakarta 12980</address>
+              </div>
+              <div class="widget">
+                <h4 class="widget-title custom-font-white mb-3">Get in Touch</h4>
+                <a href="mailto:marketing@gsilab.id"><img src="<?php echo base_url('template') ?>/assets/img/gsi/email2.png" class="img fluid w-5" alt="" /> marketing@gsilab.id</a><br>
+                <a href="https://wa.me/628111965699" target="_blank"><img src="<?php echo base_url('template') ?>/assets/img/gsi/wa.png" class="img fluid w-5" alt="" /> 0811-1965-699</a>
               </div>
               <!-- /.widget -->
             </div>
             <!-- /column -->
             <div class="col-md-4 col-lg-2 ">
               <div class="widget">
-                <p class="mb-4">Follow for More.</p>
+                <h4 class="widget-title mb-4 custom-font-white">Follow for More</h4>
                 <nav class="nav social social-white">
-                  <a href="#"><i class="uil uil-twitter"></i></a>
-                  <a href="#"><i class="uil uil-facebook-f"></i></a>
-                  <a href="#"><i class="uil uil-dribbble"></i></a>
-                  <a href="#"><i class="uil uil-instagram"></i></a>
-                  <a href="#"><i class="uil uil-youtube"></i></a>
+                  <a href="https://www.instagram.com/gsilab.id/" target="_blank"><img src="<?php echo base_url('template')?>/assets/img/gsi/instagram.png" class="img fluid w-5" alt="" /></a><br>
+                  <a href="https://www.facebook.com/GSILAB.ID/" target="_blank"><img src="<?php echo base_url('template')?>/assets/img/gsi/facebook.png" class="img fluid w-5" alt="" /></a><br>
+                  <a href="https://www.tiktok.com/@gsilab.id" target="_blank"><img src="<?php echo base_url('template')?>/assets/img/gsi/tiktok.png" class="img fluid w-5" alt="" /></a><br>
+                  <a href="https://www.linkedin.com/company/gsi-lab/mycompany/" target="_blank"><img src="<?php echo base_url('template')?>/assets/img/gsi/linkind.png" class="img fluid w-5" alt="" /></a><br>
                 </nav>
                 <!-- /.social -->
               </div>
